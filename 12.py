18954530ba30f795863b36c035eb45b2c559e684
umid bobolov <93453081+umid1991@users.noreply.github.com>
2023-03-08 23:37:09 +0500

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,14 +1,54 @@
-narh = 15000
-choy = True
-salat = True
-sal  = True
-print("salom")
+car0 = {
+        'model':'lasit',
+        'rang':'op',
+        'yil':'2008',
+        'narh':'1300',
+        'km':'5000',
+        'karopka':'avtomat'
+        }
 
-if choy and salat:
-    narh = narh + 10000
-elif choy or salat:
-    narh = narh + 5000
-elif choy or salat and sal:
-    narh = narh + 5000 + 5000
-    
-print(f"Jami {narh} sum")
+car1 = {
+        'model':'nehiy',
+        'rang':'op',
+        'yil':'2008',
+        'narh':'1300',
+        'km':'5000',
+        'karopka':'avtomat'
+        }
+
+car2 = {
+        'model':'gentra',
+        'rang':'op',
+        'yil':'2008',
+        'narh':'1300',
+        'km':'5000',
+        'karopka':'avtomat'
+        }
+
+# car = car0
+# print(f"{car['model'].title()}, "
+#       f"{car['rang']} rang, "
+#       f"{car['yil']}-yil, {car['narh']}$"
+#       )
+
+
+cars = [car0, car1, car2]
+
+# for car in cars:
+#     print(f"{car['model'].title()}, "
+#            f"{car['rang']} rang, "
+#            f"{car['yil']}-yil, {car['narh']}$"
+#            )
+
+dasturchi = {
+    'ali':['python', 'c++'],
+    'vali':['html', 'c++'],
+    'shali':['php', 'c++'],
+    'ahmat':['c==', 'c++'],
+    'tuhmat':['python', 'c++'],
+    }
+
+for ism, tillar in dasturchi.items():
+    print(f"\n {ism.title()} quydagi daturlash tillarin biladi")
+    for til in tillar:
+        print(til.upper())
